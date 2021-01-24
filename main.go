c4377d81ea1cb2fa518691d19400e7e70bae85e3
Fumiya Sato <fumist1028@gmail.com>
2021-01-24 21:47:37 +0900

    added listenAndServe

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"net/http"
+	"practices/github.com/labstack/gommon/log"
+
+)
+
 func main() {
 
+	r := router.Router()
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
