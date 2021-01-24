24a79218702b7b01ceab96b293f01e1f8220f063
Fumiya Sato <fumist1028@gmail.com>
2021-01-24 21:48:09 +0900

    added db schema of user

---
+++ mysql/init_db/user_schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `go_db`;
+USE `go_db`;
+
+CREATE TABLE IF NOT EXISTS users(
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL,
+    `email` VARCHAR(256) NOT NULL,
+    `token` VARCHAR(256) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP ON UPDATE ON UPDATE CURRENT_TIMESTAMP
+)ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO users(name, email, token) Value('satofumi', 'aaa@gmail.com', 'abc123')
