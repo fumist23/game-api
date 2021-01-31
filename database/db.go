963854a145d6e49a495600621c6c16df9720d177
Fumiya Sato <fumist1028@gmail.com>
2021-01-31 22:32:19 +0900

    fixed dbport

--- database/db.go
+++ database/db.go
@@ -10,7 +10,7 @@ import (
 var DB *sql.DB
 
 func init() {
-	db, err := sql.Open("mysql", "user:password@tcp(db:3306)/go_db?parseTime=true")
+	db, err := sql.Open("mysql", "user:password@tcp(db:13306)/go_db?parseTime=true")
 	if err != nil {
 		log.Printf("db init error: %v", err)
 	}
