44831e8c3b98e4eeab5257f2de313fcc55ad9360
Fumiya Sato <fumist1028@gmail.com>
2021-02-08 10:44:27 +0900

    added further handleFunc

--- router/router.go
+++ router/router.go
@@ -10,6 +10,7 @@ func Router() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/user/create", controller.CreateUser)
 	r.HandleFunc("/user/get", controller.GetUser)
+	r.HandleFunc("/user/update", controller.UpdateUser)
 
 	return r
 }
