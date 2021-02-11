e8ee9ab1d9ee0c8ab7efdfe14b2afb12566a1f92
Fumiya Sato <fumist1028@gmail.com>
2021-02-11 16:09:49 +0900

    added userCharacter in DB

--- model/characters.go
+++ model/characters.go
@@ -4,3 +4,8 @@ type Character struct {
 	name    string `json:"name"`
 	reality int    `json:"reality"`
 }
+
+type UserCharacter struct {
+	userID  int `json:"userId"`
+	reality int `json:"reality"`
+}
---
+++ mysql/init_db/user_characters.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS `go_db`;
+
+USE `go_db`;
+
+CREATE TABLE IF NOT EXISTS userCharacters(
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `userId` INT NOT NULL,
+    `characterId` INT NOT NULL,
+) ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
