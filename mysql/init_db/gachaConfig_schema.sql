3949db31de3ab43a502f287d8d81ac431e0d792d
Fumiya Sato <fumist1028@gmail.com>
2021-02-11 16:49:14 +0900

    fix

--- mysql/init_db/gachaConfig_schema.sql
+++ mysql/init_db/gachaConfig_schema.sql
@@ -5,7 +5,7 @@ USE `go_db`;
 CREATE TABLE IF NOT EXISTS gachaConfigs(
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `reality` INT NOT NULL,
-    `probability` DECIMAL NOT NULL
+    `probability` FLOAT NOT NULL
 )ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO gachaConfigs(reality, probability) VALUE(5, 0.01);
