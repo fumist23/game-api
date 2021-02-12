1ccc2eae972b11cbf1d255701e1f68033ccfa300
Fumiya Sato <fumist1028@gmail.com>
2021-02-12 22:00:39 +0900

    updated

--- database/sql_handler.go
+++ database/sql_handler.go
@@ -43,8 +43,7 @@ func GetUser(ctx context.Context, token string) (string, error) {
 
 // tokenとnameを受け取ってtokenに該当するuserのnameを更新する
 func UpdateUser(ctx context.Context, token string, name string) error {
-	_, err := DB.QueryContext(ctx, "UPDATE users SET name = ? WHERE token = ? ", name, token)
-	if err != nil {
+	if _, err := DB.QueryContext(ctx, "UPDATE users SET name = ? WHERE token = ? ", name, token); err != nil {
 		log.Printf("failed to update user: %v", err)
 		return err
 	}
